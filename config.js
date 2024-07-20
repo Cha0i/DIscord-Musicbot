2aecc9d0018bf3751bb67ba62d23b5b60110b2dc
Cha0i <tom@txcdn.nl>
2024-07-20 04:10:33 +0200

    Initial commit

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	token: process.env.token || "MTI2MjgxNjEzNDE2MDMxODYxNQ.GeVRaS.xCyytQsJIDxm34TgwLkxm6nZcHkiEIyIhm2OL4", //- Bot's Token
+	clientId: process.env.clientId || "1262816134160318615", //- ID of the bot
+	clientSecret: process.env.clientSecret || "tcFC8zimY8b2eVzvpUXiK6fv5HHS2dAq", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
